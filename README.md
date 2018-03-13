765c99f317736c48b1b18ad5e34cf2dd05cd5ddc
Benoit Bovy <benbovy@gmail.com>
2018-03-14 00:00:29 +0100

    fix projection/resampling issue (using pyresample)

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Visualising flows from LEMs using leaflet-velocity
 
 ## Instructions to run the demo
 
-An example of input is given with `velocity_field.hdr` and
+An example of input is given with the files `velocity_field.hdr` and
 `velocity_field.txt`. The former contains grid metadata (ESRI ASCII
-grid format + additional entry that specifies the projection system
-used), and the latter contains velocity data (two columns: for `u` and `v`
-components)
+grid format + an additional entry that specifies the projection system
+used), and the latter contains velocity data (two columns, i.e., for
+`u` and `v` components)
 
 Run the python script to convert the input data into a
-leaflet-velocity compatible (json) format (you need to have Python 3,
-Pandas and PyProj installed in your system):
+leaflet-velocity compatible (json) format (you need Python 3, Pandas,
+PyProj and pyresample installed in your system):
 
 ```
 $ python velocity2json.py velocity_field
--- velocity2json.py
+++ velocity2json.py
@@ -5,45 +5,73 @@ import argparse
 import os
 
 import pandas as pd
-import numpy as np
 from pyproj import Proj
+from pyresample import image, geometry
+
+
+def resample(field, nrows, ncols, src_area, dest_area):
+    field2d = field.reshape((nrows, ncols))
+
+    src_container = image.ImageContainerQuick(field2d, src_area)
+    dest_container = src_container.resample(dest_area)
+
+    resampled = dest_container.image_data
+
+    return resampled.flatten()
 
 
 def main(args):
     # read data file
     df = pd.read_csv('{}.txt'.format(args.input_basename),
                      names=['u', 'v'])
-    
+
     # read header file
     hdr = (pd.read_csv('{}.hdr'.format(args.input_basename),
                    delim_whitespace=True, index_col=0, header=None)
            .to_dict()[1])
-    
+
+    # get source grid specifications (projected)
     yllcorner = float(hdr['yllcorner'])
     xllcorner = float(hdr['xllcorner'])
     ncols = int(hdr['ncols'])
     nrows = int(hdr['nrows'])
     cellsize = float(hdr['cellsize'])
 
-    # find lat/lon coordinates of grid bounds 
-    latlon_proj = Proj("+init={}".format(hdr['projection']))
-
     y_lower, y_upper = yllcorner, yllcorner + nrows * cellsize
     x_left, x_right = xllcorner, xllcorner + ncols * cellsize
+
+    src_area = geometry.AreaDefinition(
+        "src_roi", "projected region of interest",
+        "src_proj", {'init': hdr['projection'], 'units': 'm'},
+        ncols, nrows,
+        (x_left, y_lower, x_right, y_upper)
+    )
+
+    # get/find destination grid specifications (lon/lat)
+    latlon_proj = Proj("+init={}".format(hdr['projection']))
 
     lon_left, lat_lower = latlon_proj(x_left, y_lower, inverse=True)
     lon_right, lat_upper = latlon_proj(x_right, y_upper, inverse=True)
 
-    # rough approximation
     d_lon = (lon_right - lon_left) / ncols
     d_lat = (lat_upper - lat_lower) / nrows
-    
-    # convert data into JSON
-    v_x_data = json.loads(df.u.to_json(orient='values'))
-    v_y_data = json.loads(df.v.to_json(orient='values'))
 
-    # add headers (JSON)
-    v_x_var = {
+    dest_area = geometry.AreaDefinition(
+        'dest_roi', 'lat lon region of interest',
+        'dest_proj', {'proj': 'longlat', 'ellps': 'WGS84', 'datum': 'WGS84'},
+        ncols, nrows,
+        (lon_left, lat_lower, lon_right, lat_upper)
+    )
+
+    # resample u, v velocity components : source grid -> destination grid
+    df['u_resampled'] = resample(df.u.values, nrows, ncols, src_area, dest_area)
+    df['v_resampled'] = resample(df.v.values, nrows, ncols, src_area, dest_area)
+
+    # prepare leaflet-velocity compatible (data + headers)
+    u_data = json.loads(df.u_resampled.to_json(orient='values'))
+    v_data = json.loads(df.v_resampled.to_json(orient='values'))
+
+    u_var = {
         "header": {
             "parameterUnit": "m.s-1",
             "parameterNumber": 2,
@@ -53,14 +81,14 @@ def main(args):
             "la2": lat_lower,
             "parameterCategory": 2,
             "lo2": lon_right,
-            "nx": 383,
-            "ny": 142,
+            "nx": ncols,
+            "ny": nrows,
             "refTime": "2017-02-01 23:00:00",
             "lo1": lon_left
             },
-        'data': v_x_data}
+        'data': u_data}
 
-    v_y_var = {
+    v_var = {
         "header": {
             "parameterUnit": "m.s-1",
             "parameterNumber": 3,
@@ -70,37 +98,38 @@ def main(args):
             "la2": lat_lower,
             "parameterCategory": 2,
             "lo2": lon_right,
-            "nx": 383,
-            "ny": 142,
+            "nx": ncols,
+            "ny": nrows,
             "refTime": "2017-02-01 23:00:00",
             "lo1": lon_left
             },
-        'data': v_y_data}
+        'data': v_data}
 
-    velocity_field = [v_x_var, v_y_var]
-    
-    # write JSON file 
-    prj_root_path = os.path.dirname(os.path.realpath(__file__)) 
+    velocity_field = [u_var, v_var]
+
+    # write JSON file in demo folder
+    prj_root_path = os.path.dirname(os.path.realpath(__file__))
     out_path = os.path.join(prj_root_path,
                             'demo',
                             'velocity-field.json')
-    
+
     with open(out_path, 'w') as outfile:
         json.dump(velocity_field, outfile)
-    
-    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=("Convert velocities (text format) into "
-                     "a leaflet-velocity compatible (json) format."
+                     "a leaflet-velocity compatible (json) format, "
+                     "after resampled the original data grid onto "
+                     "the one used by leaflet. "
                      "Creates a new file 'velocity-field.json' "
                      "in the demo directory")
     )
     parser.add_argument("input_basename",
                         help=("basename of input files (two files must exist "
                               "with .txt and .hdr extensions)"))
-    
+
     args = parser.parse_args()
-                        
+
     main(args)
-    
